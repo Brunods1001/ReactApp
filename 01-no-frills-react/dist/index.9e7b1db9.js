f9afcc79e0d0d7af6927d8f93e408b9acf7ad0a8
Brunods10 <brunods1001@gmail.com>
2022-02-28 11:06:13 -0500

    Added parcel to create dist dir

---
+++ 01-no-frills-react/dist/index.9e7b1db9.js
@@ -0,0 +1,52 @@
+const Pet = (props)=>{
+    return React.createElement("div", {
+    }, [
+        React.createElement("h1", {
+        }, props.name),
+        React.createElement("h2", {
+        }, props.species),
+        React.createElement("h2", {
+        }, props.breed), 
+    ]);
+};
+const Mimi = {
+    name: "Mimi",
+    species: "Hamster",
+    breed: "Syrian"
+};
+const Mojo = {
+    name: "Mojo",
+    species: "Dog",
+    breed: "Bichon"
+};
+const Luna = {
+    name: "Luna",
+    species: "Dog",
+    breed: "Havanese"
+};
+const Quickplay = {
+    name: "Quickplay",
+    species: "Cat",
+    breed: "?"
+};
+const createPet = (props)=>{
+    return React.createElement(Pet, props);
+};
+const petProps = [
+    Mimi,
+    Mojo,
+    Luna,
+    Quickplay
+];
+const elements = petProps.map(createPet);
+const App = ()=>{
+    return React.createElement("div", {
+    }, [
+        React.createElement("h1", {
+        }, "Adopt me!"),
+        elements, 
+    ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+//# sourceMappingURL=index.9e7b1db9.js.map
--- 01-no-frills-react/src/App.js
+++ 01-no-frills-react/src/App.js
@@ -1,20 +1,48 @@
-const Pet = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Luna"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Havanese"),
-    ]);
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.species),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
 
+const Mimi = {
+  name: "Mimi",
+  species: "Hamster",
+  breed: "Syrian",
+};
+
+const Mojo = {
+  name: "Mojo",
+  species: "Dog",
+  breed: "Bichon",
+};
+
+const Luna = {
+  name: "Luna",
+  species: "Dog",
+  breed: "Havanese",
+};
+
+const Quickplay = {
+  name: "Quickplay",
+  species: "Cat",
+  breed: "?",
+};
+
+const createPet = (props) => {
+  return React.createElement(Pet, props);
+};
+
+const petProps = [Mimi, Mojo, Luna, Quickplay];
+
+const elements = petProps.map(createPet);
+
 const App = () => {
-    return React.createElement("div", {}, 
-        [
-        React.createElement("h1", {}, "Adopt me!"),
-            Pet(),
-            Pet(),
-            Pet(),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    elements,
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
